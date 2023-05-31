0c2c7f5dbd9fb4734a72a008ac64c1facbe8fce2
marengoo09 <valentinmarengo09@gmail.com>
2023-05-31 14:48:54 -0300

    some accessibility changes

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -9,7 +9,6 @@ import iconLight from '../../../public/images/icon-moon.svg'
 import iconDark from '../../../public/images/icon-sun.svg'
 import { ThemeContext } from "../../contexts/themeContext";
 
-
 export const Header = ({children}) => {
   const {theme, setTheme} = useContext(ThemeContext)
   const handleClick = () => {
@@ -20,7 +19,7 @@ export const Header = ({children}) => {
       <div className={`content theme-dark`}>
         <div className='header-content'>
           <h1>TODO</h1>
-          <img onClick={handleClick} src={(theme==='light'?iconLight:iconDark)}/>
+          <img onClick={handleClick} src={(theme==='light'?iconLight:iconDark)} alt="Toggle between dark and light mode"/>
         </div>
         {children}
       </div>
